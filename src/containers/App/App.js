e35bb72be0b549198e6a48e16da6ae2e96216caa
Vitalii Hudanets <wburn@hostivar-0217.koleje.cuni.cz>
2019-05-12 18:16:37 +0200

    Section Banner
    Only visual part without slider logic

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Banner from '../Banner/Banner';
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <Banner />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/containers/Banner/Banner.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Divider from '../../components/Divider/Divider';
+
+function Banner() {
+    return (
+        <section className="Banner">
+            <div className="Banner__Slider">
+                <span className="Banner__Slider-title">creative template</span>
+                <span className="Banner__Slider-text">welcome</span>
+                <span className="Banner__Slider-text">to mogo</span>
+                <Divider />
+                <div className="Banner__Slider-link">
+                    <a href='localhost:3000'>learn more</a>
+                </div>
+            </div>
+            <div className="Banner__SliderControls">
+                <div>
+                    <span className="Banner__SliderControls-number">01</span>
+                    <span className="Banner__SliderControls-text">intro</span>
+                </div>
+                <div>
+                    <span className="Banner__SliderControls-number">02</span>
+                    <span className="Banner__SliderControls-text">work</span>
+                </div>
+                <div>
+                    <span className="Banner__SliderControls-number">03</span>
+                    <span className="Banner__SliderControls-text">about</span>
+                </div>
+                <div>
+                    <span className="Banner__SliderControls-number">04</span>
+                    <span className="Banner__SliderControls-text">contacts</span>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Banner;
