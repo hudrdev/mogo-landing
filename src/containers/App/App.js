50ea5c2bde56ff2fde8f955ca951a68fcd946e65
Vitalii Hudanets <wburn@hostivar-0217.koleje.cuni.cz>
2019-05-18 00:53:41 +0200

    Base changes
    imports added, grid-areas added

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import Banner from '../Banner/Banner';
+import AboutUs from '../AboutUs/AboutUs';
+import Facts from '../Facts/Facts';
+import Services from '../Services/Services';
 
 function App() {
     return (
         <div className="App">
             <Banner />
+            <AboutUs />
+            <Facts />
+            <Services />
         </div>
     );
 }
