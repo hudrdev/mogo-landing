b39a313f9a61fed728d35093361c0750b0c2ffbc
Vitalii Hudanets <wburn@hostivar-0217.koleje.cuni.cz>
2019-05-18 00:51:16 +0200

    Section Facts

---
+++ src/containers/Facts/Facts.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Facts = () => (
+    <div className="Facts">
+        <div className="Facts__Container">
+            <div className="Facts__Container-item">
+                <span className="Facts__Container-number">42</span>
+                <p className="Facts__Container-text">web design projects</p>
+            </div>
+            <div className="Facts__Container-item">
+                <span className="Facts__Container-number">123</span>
+                <p className="Facts__Container-text">happy client</p>
+            </div>
+            <div className="Facts__Container-item">
+                <span className="Facts__Container-number">15</span>
+                <p className="Facts__Container-text">award winner</p>
+            </div>
+            <div className="Facts__Container-item">
+                <span className="Facts__Container-number">99</span>
+                <p className="Facts__Container-text">cup of coffee</p>
+            </div>
+            <div className="Facts__Container-item">
+                <span className="Facts__Container-number">24</span>
+                <p className="Facts__Container-text">members</p>
+            </div>
+        </div>
+    </div>
+)
+
+export default Facts;
