4d0596b5873f6eef31f30b10c548c7ccfbe8a7c5
Vitalii Hudanets <wburn@hostivar-0217.koleje.cuni.cz>
2019-05-19 23:21:42 +0200

    Section Logos

---
+++ src/containers/Logos/Logos.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Logos = () => (
+    <div className="Logos">
+        <div className="Logos__Container">
+            <img src={require('../../img/logo-1.jpg')} alt=""/>
+            <img src={require('../../img/logo-2.jpg')} alt=""/>
+            <img src={require('../../img/logo-3.jpg')} alt=""/>
+            <img src={require('../../img/logo-4.jpg')} alt=""/>
+            <img src={require('../../img/logo-5.jpg')} alt=""/>
+            <img src={require('../../img/logo-6.jpg')} alt=""/>
+        </div>
+    </div>
+)
+
+export default Logos;
